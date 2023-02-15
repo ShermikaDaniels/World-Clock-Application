8c582e188c0c329d3d9e5ac85644c4dfff1ad748
ShermikaDaniels <112131028+ShermikaDaniels@users.noreply.github.com>
2023-02-15 18:45:05 -0500

    Added the append feature

--- javascript/index.js
+++ javascript/index.js
@@ -31,7 +31,7 @@ function updateCity(event) {
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
+  citiesElement.innerHTML += `
   <div class="city">
   <h2>${cityName}</h2>
   <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
